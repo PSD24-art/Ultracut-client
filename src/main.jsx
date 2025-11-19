2dd4153992f9aace8bc30ac3030159fe2af68c60
Prathamesh <pratham.dahakepsd@gmail.com>
2025-11-19 15:16:34 +0530

    Switched back to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
 import "./index.css";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
